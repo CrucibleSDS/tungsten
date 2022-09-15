4ce0d365cd43eeb7eedb666fb0cbedb1a9daa90b
Dennis Pham <dennis@dennispham.me>
2022-09-15 12:10:30 -0400

    Install `pytest` as a dev dependency.

---
+++ tests/test_sigma_aldrich.py
@@ -0,0 +1,14 @@
+class TestSigmaAldrich:
+    filename = "tests/samples/sigma_aldrich_10.pdf"
+
+    def test_product_identifier(self):
+        pass
+
+    def test_identified_uses_or_uses_advised_against(self):
+        pass
+
+    def test_details_of_supplier(self):
+        pass
+
+    def test_emergency_telephone_number(self):
+        pass
