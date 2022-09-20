415e3beb8c43e0428b90dccb1775f3f1d4644746
Dennis Pham <dennis@dennispham.me>
2022-09-20 12:14:57 -0400

    Add WIP sigma aldrich parser.

--- tungsten/__main__.py
+++ tungsten/__main__.py
@@ -1,2 +1,9 @@
+from tungsten.parsers.sigma_aldrich import parse_sigma_aldrich
+
+
+def main() -> None:
+    parse_sigma_aldrich("tests/samples/sigma_aldrich_sigma_w5402.pdf")
+
+
 if __name__ == "__main__":
-    print("Hello, world!")
+    main()
