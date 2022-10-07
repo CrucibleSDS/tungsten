f81e1be70364e412f98ebdbcff2a1c7855cfa232
Brian Lu <me@greencappuccino.net>
2022-10-07 01:33:27 -0400

    scan samples and output

--- tungsten/__main__.py
+++ tungsten/__main__.py
@@ -1,15 +1,24 @@
 import json
+import os
+import time
 
 from tungsten.parsers.globally_harmonized_system import GhsSdsJsonEncoder
 from tungsten.parsers.sigma_aldrich import parse_sigma_aldrich
 from dataclasses import asdict;
 
+
 def main() -> None:
-    file = open("output.json", "w")
-    # noinspection PyTypeChecker
-    ghssds = parse_sigma_aldrich(open("tests/samples/sigma_aldrich_sigma_w5402.pdf", "rb", buffering=0))
-    json.dump(asdict(ghssds), file, cls=GhsSdsJsonEncoder, skipkeys=True)
-    file.close()
+    files = os.listdir("./tests/samples/")
+    paths = ["./tests/samples/" + file for file in files]
+
+    for i in range(len(paths)):
+        start = time.perf_counter()
+        # noinspection PyTypeChecker
+        ghssds = parse_sigma_aldrich(open(paths[i], "rb", buffering=0))
+        file = open("./output/" + files[i].split(sep=".")[0] + ".json", "w")
+        json.dump(asdict(ghssds), file, cls=GhsSdsJsonEncoder, skipkeys=True)
+        file.close()
+        print(time.perf_counter() - start, "seconds")
 
 
 if __name__ == "__main__":
