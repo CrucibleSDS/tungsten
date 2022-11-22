057e2020419da70bef5062db46e274d4e5dff3cd
Brian Lu <me@greencappuccino.net>
2022-11-22 03:03:34 -0500

    use absolute jar path

--- tungsten/__init__.py
+++ tungsten/__init__.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from tungsten.parsers.globally_harmonized_system.safety_data_sheet import (
     GhsSdsJsonEncoder
@@ -7,6 +8,7 @@ from tungsten.parsers.supplier.sigma_aldrich.sds_parser import (
     SigmaAldrichSdsParser
 )
 
-__all__ = ("GhsSdsJsonEncoder", "SigmaAldrichSdsParser")
+os.environ["TABULA_JAR"] = str(
+    (Path(__file__).parent.parent / "tabula-1.0.6-SNAPSHOT-jar-with-dependencies.jar").resolve())
 
-os.environ["TABULA_JAR"] = "tabula-1.0.6-SNAPSHOT-jar-with-dependencies.jar"
+__all__ = ("GhsSdsJsonEncoder", "SigmaAldrichSdsParser")
