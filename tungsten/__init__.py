e4e243131746e27012d45dada13ed7c7a428bfe4
Brian Lu <me@greencappuccino.net>
2022-11-22 02:36:05 -0500

    include jar in project

--- tungsten/__init__.py
+++ tungsten/__init__.py
@@ -1,6 +1,12 @@
-from tungsten.parsers.globally_harmonized_system.safety_data_sheet import \
+import os
+
+from tungsten.parsers.globally_harmonized_system.safety_data_sheet import (
     GhsSdsJsonEncoder
-from tungsten.parsers.supplier.sigma_aldrich.sds_parser import \
+)
+from tungsten.parsers.supplier.sigma_aldrich.sds_parser import (
     SigmaAldrichSdsParser
+)
 
 __all__ = ("GhsSdsJsonEncoder", "SigmaAldrichSdsParser")
+
+os.environ["TABULA_JAR"] = "tabula-1.0.6-SNAPSHOT-jar-with-dependencies.jar"
